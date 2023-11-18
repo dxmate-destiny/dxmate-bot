597466d47f4482c5eb5497cdfc33e334ecf6b3c1
dxmate-destiny <info.dxmate@gmail.com>
2023-11-18 22:45:37 +0900

    Fix profile command

--- src/commands/player/profile.js
+++ src/commands/player/profile.js
@@ -70,14 +70,14 @@ module.exports = {
         .setColor(0x0099FF)
         .setAuthor({ name: discordUserData.name, iconURL: discordUserData.avatarUrl })
         .addFields(
-            { name: 'Slippi Connect Code', value: getDxmatePlayerDataResponse.data.slippiConnectCode, inline: true },
-            { name: 'Current Region', value: regionChoices[getDxmatePlayerDataResponse.data.region], inline: true },
             { name: 'Singles Rank', value: getSinglesRankDataResponse.data.name, inline: true },
             { name: 'Singles Rank Points', value: getSinglesRankDataResponse.data.points + ' RP', inline: true },
             { name: 'Ranked Singles Count', value: getDxmatePlayerDataResponse.data.rankedModeMatchCount.singles.toString(), inline: true },
             { name: 'Doubles Rank', value: getDoublesRankDataResponse.data.name, inline: true },
             { name: 'Doubles Rank Points', value: getDoublesRankDataResponse.data.points + ' RP', inline: true },
             { name: 'Ranked Doubles Count', value: getDxmatePlayerDataResponse.data.rankedModeMatchCount.doubles.toString(), inline: true },
+            { name: 'Slippi Connect Code', value: getDxmatePlayerDataResponse.data.slippiConnectCode, inline: true },
+            { name: 'Current Region', value: regionChoices[getDxmatePlayerDataResponse.data.region], inline: true }
         );
 
         // Send DXmate player profile embed.
